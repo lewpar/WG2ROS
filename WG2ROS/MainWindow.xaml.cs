68157d5e26db4ea59f6d02f396d04fa12abc2ebe
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-05-30 19:21:31 +1000

    Implemented file loading.

--- WG2ROS/MainWindow.xaml.cs
+++ WG2ROS/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,51 @@ namespace WG2ROS
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBoxServerConfigPath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text = TextBoxServerConfigPath.Text;
+
+            LabelBrowsePlaceholder.Visibility = string.IsNullOrEmpty(text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void TextBoxServerConfigPath_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void TextBoxServerConfigPath_PreviewDrop(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+                TextBoxServerConfigPath.Text = paths[0];
+                GetConfig();
+            }
+        }
+
+        private void GetConfig()
+        {
+            if(!File.Exists(TextBoxServerConfigPath.Text))
+            {
+                return;
+            }
+
+            var cfg = File.ReadAllText(TextBoxServerConfigPath.Text);
+
+            TextBoxPreviewConfig.Text = cfg;
+        }
+
+        private void ButtonBrowseServerConfig_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            var result = ofd.ShowDialog();
+            if(result.HasValue && result.Value)
+            {
+                TextBoxServerConfigPath.Text = ofd.FileName;
+                GetConfig();
+            }
         }
     }
 }
